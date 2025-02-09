8c7c2ad5d21f3a861b0547d443b3d5031171f6e6
ssamyn <stefaan.samyn@hogent.be>
2025-02-09 20:52:04 +0100

    Voorbeelden toSorted, toSpliced en toReversed toegevoegd

--- voorbeeldenSlides/02arrays/js/arrays.js
+++ voorbeeldenSlides/02arrays/js/arrays.js
@@ -9,5 +9,32 @@ console.log(pizzas.join());
 console.log(pizzas.slice(2, 4));
 console.log(pizzas.splice(2, 1, "Chicken & Pepper", "Veggie Deluxe"));
 console.log(pizzas);
-console.log(pizzas.reverse());
 console.log(pizzas.sort());
+
+// reverse vs toReversed
+pizzas = ["Chicken & Bacon", "Mushroom", "Pepperoni"];
+pizzas.reverse();
+console.log(pizzas);
+pizzas = ["Chicken & Bacon", "Mushroom", "Pepperoni"];
+const reversedPizzas = pizzas.toReversed();
+console.log(pizzas);
+console.log(reversedPizzas);
+
+// splice vs toSpliced
+pizzas = [
+  "Margherita",
+  "Mushroom",
+  "Spinach & Rocket",
+  "Pineapple & Sweetcorn",
+];
+pizzas.splice(1, 2, "Hawai");
+console.log(pizzas);
+pizzas = [
+  "Margherita",
+  "Mushroom",
+  "Spinach & Rocket",
+  "Pineapple & Sweetcorn",
+];
+const splicedPizzas = pizzas.toSpliced(1, 2, "Hawai");
+console.log(pizzas);
+console.log(splicedPizzas);
